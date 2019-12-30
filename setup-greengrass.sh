9cf7431d818cd24570e795187aa91cef2bd72c00
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-30 10:30:27 +0900

    add setup-greengrass.sh

---
+++ setup-greengrass.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script=gg-device-setup-latest.sh
+url=https://d1onfpft10uf5o.cloudfront.net/greengrass-device-setup/downloads/$script
+option=bootstrap-greengrass
+region=us-east-1
+new=new-$script
+output=output.txt
+dir=/home/ubuntu
+
+cd $dir
+wget -q -O ./$script $url
+sed -e s/us-west-2/$region/g $script > $new
+chmod +x ./$new
+./$new $option > $output
