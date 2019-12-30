f0306058d28d4d422fd4983bd1f2678490621636
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-30 10:23:36 +0900

    add set-credentials.sh

---
+++ set-credentials.sh
@@ -0,0 +1,10 @@
+apt update
+apt install -y jq
+
+metadata=http://169.254.169.254/latest/meta-data
+role="curl $metadata/iam/security-credentials"
+cred=$(echo $role/$($role))
+
+export AWS_ACCESS_KEY_ID=$($cred | jq -r .AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$($cred | jq -r .SecretAccessKey)
+export AWS_SESSION_TOKEN=$($cred | jq -r .Token)
