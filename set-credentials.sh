badde7de96bce034c75cb5b1a2b74d76778eaa79
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-30 20:31:02 +0900

    add -s option to curl command

--- set-credentials.sh
+++ set-credentials.sh
@@ -4,7 +4,7 @@ apt update
 apt install -y jq
 
 metadata=http://169.254.169.254/latest/meta-data
-role="curl $metadata/iam/security-credentials"
+role="curl -s $metadata/iam/security-credentials"
 cred=$(echo $role/$($role))
 
 export AWS_ACCESS_KEY_ID=$($cred | jq -r .AccessKeyId)
