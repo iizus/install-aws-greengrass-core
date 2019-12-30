1ee504d7ae7dd79ce18395d4ca79ffe6f26db1c3
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-30 20:51:55 +0900

    fix

--- main.sh
+++ main.sh
@@ -4,13 +4,22 @@ apt update
 apt install -y ec2-instance-connect
 
 base=https://raw.githubusercontent.com/iizus/setup-greengrass-installed-ec2-instance/master
-scripts=(
-    "set-credentials"
-    "setup-greengrass"
-    "set-tag"
-)
+# scripts=(
+#     "set-credentials"
+#     "setup-greengrass"
+#     "set-tag"
+# )
 
-for script in "${scripts[@]}" ; do
-    url=$base/$script.sh
-    curl -s $url | bash
-done
+# for script in "${scripts[@]}" ; do
+#     url=$base/$script.sh
+#     curl -s $url | bash
+# done
+
+url=$base/set-credentials.sh
+curl -s $url | bash
+
+url=$base/setup-greengrass.sh
+curl -s $url | bash
+
+url=$base/set-tag.sh
+curl -s $url | bash
