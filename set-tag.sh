940e2d18bd1863d5188caa0d7ee84b74624704f0
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-30 10:53:59 +0900

    add set-tag.sh

---
+++ set-tag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+metadata=http://169.254.169.254/latest/meta-data
+id=$(curl $metadata/instance-id)
+group=$(grep "Greengrass group name" $output)
+name=$(echo $group | cut -c 33-)
+
+apt update
+apt install -y awscli
+aws ec2 create-tags --resources $id --tags Key=Name,Value=$name --region $region
