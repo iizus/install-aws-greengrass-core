c9e7dceeefee442373ba1f6ba8da60d4a38a33d3
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-31 14:21:21 +0900

    add all

--- install.sh
+++ install.sh
@@ -34,12 +34,11 @@ chmod +x ./$new
 
 
 
+metadata=http://169.254.169.254/latest/meta-data
+id=$(curl -s $metadata/instance-id)
+group=$(grep "Greengrass group name" $output)
+name=$(echo $group | cut -c 33-)
 
-repo=setup-greengrass-installed-ec2-instance
-git clone https://github.com/iizus/$repo.git
-
-cd $repo
-chmod +x *
-
-./setup-greengrass.sh bash set-credentials.sh
-./set-tag.sh bash set-credentials.sh
+apt update
+apt install -y awscli
+aws ec2 create-tags --resources $id --tags Key=Name,Value=$name --region $region
