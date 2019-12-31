be9d2dd3257ed1e447cfc2532b3b55e6d2d2704f
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-31 10:25:02 +0900

    add chmod

--- main.sh
+++ main.sh
@@ -5,7 +5,9 @@ apt install -y ec2-instance-connect
 
 repo=setup-greengrass-installed-ec2-instance
 git clone https://github.com/iizus/$repo.git
+
 cd $repo
+chmod +x *
 
 ./setup-greengrass.sh bash set-credentials.sh
 ./set-tag.sh bash set-credentials.sh
