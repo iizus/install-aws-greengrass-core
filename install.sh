0be9b89e3f1cedc72c389dbbbd6fc92238d6b7fb
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-31 13:04:40 +0900

    add

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@
 apt update
 apt install -y ec2-instance-connect
 
+apt install -y jq
+
+metadata=http://169.254.169.254/latest/meta-data
+role="curl -s $metadata/iam/security-credentials"
+cred=$(echo $role/$($role))
+
+export AWS_ACCESS_KEY_ID=$($cred | jq -r .AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$($cred | jq -r .SecretAccessKey)
+export AWS_SESSION_TOKEN=$($cred | jq -r .Token)
+
+
+
+
 repo=setup-greengrass-installed-ec2-instance
 git clone https://github.com/iizus/$repo.git
 
