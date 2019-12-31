b424493e26710c4ed0f1e100d75fab0d1940c12e
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2019-12-31 13:06:00 +0900

    add

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 apt update
 apt install -y ec2-instance-connect
 
+
+
 apt install -y jq
 
 metadata=http://169.254.169.254/latest/meta-data
@@ -12,6 +14,23 @@ cred=$(echo $role/$($role))
 export AWS_ACCESS_KEY_ID=$($cred | jq -r .AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$($cred | jq -r .SecretAccessKey)
 export AWS_SESSION_TOKEN=$($cred | jq -r .Token)
+
+
+
+script=gg-device-setup-latest.sh
+url=https://d1onfpft10uf5o.cloudfront.net/greengrass-device-setup/downloads/$script
+option=bootstrap-greengrass
+region=us-east-1
+new=new-$script
+output=output.txt
+dir=/tmp
+
+cd $dir
+wget -q -O ./$script $url
+sed -e s/us-west-2/$region/g $script > $new
+chmod +x ./$new
+./$new $option > $output
+
 
 
 
