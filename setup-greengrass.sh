33cabbbfb09509f16e15b48405de79c4b70fb3ab
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-03-03 21:58:17 +0900

    Update setup-greengrass.sh

--- setup-greengrass.sh
+++ setup-greengrass.sh
@@ -2,7 +2,7 @@
 
 script=gg-device-setup-latest.sh
 url=https://d1onfpft10uf5o.cloudfront.net/greengrass-device-setup/downloads/$script
-option=bootstrap-greengrass
+option="bootstrap-greengrass --hello-world-lambda"
 region=us-east-1
 new=new-$script
 output=output.txt
