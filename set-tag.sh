064f2aa6797246e799ce59380742c1b2c374e95b
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-03-03 22:23:41 +0900

    Update set-tag.sh

--- set-tag.sh
+++ set-tag.sh
@@ -6,6 +6,4 @@ group=$(grep "Greengrass group name" $output)
 name=$(echo $group | cut -c 33-)
 region=us-east-1
 
-apt update
-apt install -y awscli
 aws ec2 create-tags --resources $id --tags Key=Name,Value=$name --region $region
